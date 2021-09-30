09c80571877eeca6ecbacecd89e856deec21ef20
Rodrigo <rodrigomartins@id.uff.br>
2021-09-30 13:20:44 -0300

    clean and well-structured, functions/methods short and readable, docstrings, type-hinting etc

--- app.py
+++ app.py
@@ -19,26 +19,42 @@ from sklearn.decomposition import PCA
 
 #Packages to manipulate and perform metrics
 from sklearn.model_selection import train_test_split
-from sklearn import model_selection
+from sklearn.metrics import mean_squared_error
 
 #Packages for Regressor
-#https://scikit-learn.org/stable/auto_examples/ensemble/plot_gradient_boosting_regression.html
-#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingRegressor.html
-#from sklearn.datasets import make_regression
+import abc
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_percentage_error
-
 from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
 
-st.title("Data Science for Mining Engineering")
+def perform_regressor(reg: abc.ABCMeta, X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame):
+	reg.fit(X_train, y_train)
+	y_predicted = reg.predict(X_test)
+	rmse = mean_squared_error(y_test, y_predicted, squared = True)
+
+	pixel = 1/plt.rcParams['figure.dpi']
+	fig, ax = plt.subplots(figsize=(800*pixel, 600*pixel))
+	plt.scatter(range(len(y_test)),abs(y_test-y_predicted), label = 'Absolute error between target and predicted')
+	plt.legend(loc="best")
+	vals = ax.get_yticks()
+	st.pyplot(fig)
+	plt.show()
+
+	st.success('$R^2$ of Gradient Boosting Regressor: '+str(reg.score(X_test, y_test)))
+	st.success("The root mean squared error (RMSE) on test set: {:.4f}".format(rmse))
+
+# Remove target column from dataframe before pre-processing (df) and after pre-processing (df_raw)
 df_target = df['target']
 df = df.drop(columns=['target'])
 df_raw = df_raw.drop(columns=['target'])
 
+
+st.title("Data Science for Mining Engineering")
+
+
 st.write("Dataframe before pre-processing.")
 st.write(df_raw.head())
+
 
 st.write("Dataframe after pre-processing.")
 st.write(df.head())
@@ -60,10 +76,11 @@ plt.show()
 st.pyplot(fig)
 
 
-st.write("Dataframe's Features Box-plot After Normalization.")
-
+# Perform max-min normalization
 df=(df-df.min())/(df.max()-df.min())
 
+
+st.write("Dataframe's Features Box-plot After Normalization.")
 fig,ax = plt.subplots(figsize=(15,15))
 sns.boxplot(data=df)
 sns.despine(offset=10, trim=True)
@@ -71,13 +88,21 @@ plt.xticks(rotation=90, fontsize=16)
 plt.show()
 st.pyplot(fig)
 
+
 st.write("Dataframe's Features Variation Explained.")
+# Instantiate PCA as instance
 pca = PCA()
 
+# Determine transformed features
 X_train_pca = pca.fit_transform(df.fillna(0))
+
+# Determine explained variance
 exp_var_pca = pca.explained_variance_ratio_
+
+# Cumulative sum of eigenvalues (to create step plot)
 cum_sum_eigenvalues = np.cumsum(exp_var_pca)
 
+# Plotting
 fig,ax = plt.subplots(figsize=(15,15))
 plt.bar(range(1,len(exp_var_pca)+1), exp_var_pca, alpha=0.5, align='center', label='Individual explained variance')
 plt.step(range(1,len(cum_sum_eigenvalues)+1), cum_sum_eigenvalues, where='mid',label='Cumulative explained variance')
@@ -91,103 +116,49 @@ plt.show()
 
 
 st.write("Dataframe's After PCA.")
+# Number of features to perform PCA
 N=15
+
+# Feature's names
 columns_vector = ['element '+str(i) for i in range(1,N+1)]
+
+# Instance PCA
 pca = PCA(n_components=N)
+
+# Perform the PCA with current DataFrame
 df_PCA = pca.fit_transform(df.fillna(0))
-df_PCA = pd.DataFrame(data = df_PCA
-             , columns = columns_vector)
+
+# Create dataframe with new features
+df_PCA = pd.DataFrame(data = df_PCA, columns = columns_vector)
+
+# Display new dataframe
 st.write(df_PCA)
 
 
-st.write("Performance of Gradient Boost Regressor")
+# Split train(67%) and test(33%)
 X_train, X_test, y_train, y_test = train_test_split(df_PCA, df_target,test_size=0.33, random_state=42)
 
+
+st.write("Performance of Gradient Boost Regressor")
 params = {'n_estimators': 500,
           'max_depth': 4,
           'min_samples_split': 5,
           'learning_rate': 0.01,
           'loss': 'ls'}
-
-reg = GradientBoostingRegressor(**params)
-reg.fit(X_train, y_train)
-y_predicted = reg.predict(X_test)
-rmse = mean_squared_error(y_test, y_predicted, squared = True)
-
-pixel = 1/plt.rcParams['figure.dpi']
-fig, ax = plt.subplots(figsize=(800*pixel, 600*pixel))
-#plt.plot(range(len(y_test)),y_test, color ='red', label = 'Target test database')
-plt.scatter(range(len(y_test)),abs(y_test-y_predicted), label = 'Absolute error between target and predicted')
-#plt.scatter(range(len(y_test)),y_predicted, label = 'Predicted')
-plt.legend(loc="best")
-#ax.set_xlabel('Temperature (Kelvin)')
-#ax.set_ylabel('Relative Error')
-vals = ax.get_yticks()
-#ax.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
-st.pyplot(fig)
-plt.show()
+perform_regressor(GradientBoostingRegressor(**params), X_train, y_train, X_test, y_test)
 
 
-st.success('$R^2$ of Gradient Boosting Regressor: '+str(reg.score(X_test, y_test)))
-st.success("The root mean squared error (RMSE) on test set: {:.4f}".format(rmse))
-
-
-st.write("Support Vector Machine")
+st.write("Performance of Support Vector Machine")
 params = {'C': 1.0,
           'degree': 9,
           'kernel': 'rbf',
           'epsilon': 0.2}       
-
-reg = SVR(**params)
-reg.fit(X_train, y_train)
-y_predicted = reg.predict(X_test)
-rmse = mean_squared_error(y_test, y_predicted, squared = True)
+perform_regressor(SVR(**params), X_train, y_train, X_test, y_test)
 
 
-pixel = 1/plt.rcParams['figure.dpi']
-fig, ax = plt.subplots(figsize=(800*pixel, 600*pixel))
-#plt.plot(range(len(y_test)),y_test, color ='red', label = 'Target test database')
-plt.scatter(range(len(y_test)),abs(y_test-y_predicted), label = 'Absolute error between target and predicted')
-#plt.scatter(range(len(y_test)),y_predicted, label = 'Predicted')
-plt.legend(loc="best")
-#ax.set_xlabel('Temperature (Kelvin)')
-#ax.set_ylabel('Relative Error')
-vals = ax.get_yticks()
-#ax.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
-st.pyplot(fig)
-plt.grid(True)
-plt.show()
-
-
-st.success('$R^2$ of Gradient Boosting Regressor: '+str(reg.score(X_test, y_test)))
-st.success("The root mean squared error (RMSE) on test set: {:.4f}".format(rmse))
-
-
-st.write("Multi-Layer Percepetron")
+st.write("Performance of Multi-Layer Percepetron")
 params = {'activation': 'relu',
           'max_iter': 500,
           'alpha': 0.005,
           'solver': 'adam'}   
-
-reg = MLPRegressor(**params)
-reg.fit(X_train, y_train)
-y_predicted = reg.predict(X_test)
-rmse = mean_squared_error(y_test, y_predicted, squared = True)
-
-pixel = 1/plt.rcParams['figure.dpi']
-fig, ax = plt.subplots(figsize=(800*pixel, 600*pixel))
-#plt.plot(range(len(y_test)),y_test, color ='red', label = 'Target test database')
-plt.scatter(range(len(y_test)),abs(y_test-y_predicted), label = 'Absolute error between target and predicted')
-#plt.scatter(range(len(y_test)),y_predicted, label = 'Predicted')
-plt.legend(loc="best")
-#ax.set_xlabel('Temperature (Kelvin)')
-#ax.set_ylabel('Relative Error')
-vals = ax.get_yticks()
-#ax.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
-st.pyplot(fig)
-plt.grid(True)
-plt.show()
-
-
-st.success('$R^2$ of Gradient Boosting Regressor: '+str(reg.score(X_test, y_test)))
-st.success("The root mean squared error (RMSE) on test set: {:.4f}".format(rmse))
+perform_regressor(MLPRegressor(**params), X_train, y_train, X_test, y_test)
--- pre_processing.py
+++ pre_processing.py
@@ -5,18 +5,36 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-"""
-Load data and merge
-"""
-def load_raw():
+def load_raw() -> pd.DataFrame:
+	"""
+	loads features needed for training dataset and target dataset to then perform a left outer join and return this single dataframe
+	"""
 	df_raw = pd.read_csv('md_raw_dataset.csv', sep = ";")
 	df_raw.rename({'Unnamed: 0': 'index'}, axis=1, inplace=True)
-	
+
 	df_target = pd.read_csv('md_target_dataset.csv', sep = ";")
 	df_target['groups'] = df_target['groups'].map(lambda x: float(x))
-	
+
 	df_raw = pd.merge(df_target, df_raw, on=['index', 'groups'], how='left')
 	return df_raw
+    
+def make_column(df: pd.DataFrame, column1: str, column2: str, new_column: str) -> pd.DataFrame:
+	"""
+	With a given dataframe (df) creates a new column (new_column) with the difference between 2 date columns
+	(column1 and column2) in terms of seconds then phase-out column1 and column2 
+	"""
+	df[column1] = df[column1].dropna().apply(lambda x: datetime.datetime.strptime(x,'%d/%m/%Y %H:%M') if type(x)==str else x)
+	df[column2] = df[column2].dropna().apply(lambda x: datetime.datetime.strptime(x,'%d/%m/%Y %H:%M') if type(x)==str else x)
+
+	df[new_column] = None
+
+	for id in df.index:
+		if df[column1][id] >= df[column2][id]: df[new_column][id] = (df[column1][id]-df[column2][id]).seconds
+		if df[column1][id] < df[column2][id]: df[new_column][id] = (df[column2][id]-df[column1][id]).seconds
+		
+	df = df.drop(columns=[column1, column2])
+
+	return df
 
 """
 Create a new dataframe as df to manipulate, and keep a raw dataframe as df_raw
@@ -77,25 +95,6 @@ then, diff_on_tower is 600 seconds
 
 
 """
-def make_column(df,column1,column2,new_column):
-    try:
-        df[column1] = df[column1].dropna().apply(lambda x: datetime.datetime.strptime(x,'%d/%m/%Y %H:%M'))
-    except:
-        pass
-    try: df[column2] = df[column2].dropna().apply(lambda x: datetime.datetime.strptime(x,'%d/%m/%Y %H:%M'))
-    except:
-        pass
-
-    df[new_column] = None
-        
-    for id in df.index:
-        if df[column1][id] >= df[column2][id]: df[new_column][id] = (df[column1][id]-df[column2][id]).seconds/60
-        if df[column1][id] < df[column2][id]: df[new_column][id] = (df[column2][id]-df[column1][id]).seconds/60
-                
-    df = df.drop(columns=[column1, column2])
-        
-    return df
-
 
 df = make_column(df,'predicted_process_end','process_end','diff_process_end')
 df = make_column(df,'start_critical_subprocess1','start_subprocess1','diff_subprocess1')
